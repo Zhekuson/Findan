3fc29142f2725fa34f8dbe446284bacdf828261d
Zhekuson <zhekuson@gmail.com>
2022-02-14 18:39:53 +0300

    some changes

--- MA_RL_OMM/macro_agent.py
+++ MA_RL_OMM/macro_agent.py
@@ -8,13 +8,13 @@ Transition = namedtuple(
     'Transition', ('state', 'action', 'reward', 'next_state', 'done'))
 
 
-class Q_network(nn.Module):
+class QNetwork(nn.Module):
     def __init__(self) -> None:
-        super(Q_network, self).__init__()
-        self.input = nn.Linear(8,8)
-        self.hidden1 = nn.Linear(8,8)
-        self.hidden2 = nn.Linear(8,8)
-        self.out = nn.Linear(8,3)
+        super(QNetwork, self).__init__()
+        self.input = nn.Linear(8, 8)
+        self.hidden1 = nn.Linear(8, 8)
+        self.hidden2 = nn.Linear(8, 8)
+        self.out = nn.Linear(8, 3)
         self.DQN = nn.Sequential(
             self.input,
             nn.ReLU(),
@@ -29,11 +29,9 @@ class Q_network(nn.Module):
         price = input['price']
         indicators = input['indicators']
         assets = input['assets']
-        diff = np.ones_like(assets) * price - assets
+        diff = np.sum(np.ones_like(assets) * price - assets)
         tensor = torch.from_numpy(np.hstack((price, indicators, diff)))
         return self.DQN(tensor)
-
-
 
 
 class ReplayMemory:
@@ -53,11 +51,7 @@ class ReplayMemory:
 
 
 class MacroAgent:
-    def __init__(self, greedy_epsilon, discount_gamma) -> None:
+    def __init__(self) -> None:
         self.assets = np.array([], dtype=float)
-        self.epsilon = greedy_epsilon
-        self.gamma = discount_gamma
-        self.Q_network = Q_network()
+        self.q_network = QNetwork()
         pass
-
-    
